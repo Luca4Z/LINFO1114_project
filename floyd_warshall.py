234c419817cc35c32fa68891e7079596f128834b
Luca <luca.stevens@icloud.com>
2024-12-05 13:52:29 +0100

    Base projet

---
+++ floyd_warshall.py
@@ -0,0 +1,12 @@
+import numpy
+
+def floyd_warshall(matrice):
+    """
+    @pre: une matrice[][] de coût
+    @post: une matrice[][] R contenant la longeur des chemins le plus court entre le noeud i et j 
+
+    # l'algorithme de Floyd-Warshall permet de calculer directement toutes les distances entre paires de noeuds.
+    """
+    # TODO
+
+    return [[]]
